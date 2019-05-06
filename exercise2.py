50d7bbe872ca83d6cba5450e40508ed91b56c4d3
Corey Crosby <ccc_13@live.ca>
2019-05-06 12:47:03 -0400

    exercise 2 completed

---
+++ exercise2.py
@@ -0,0 +1,13 @@
+#1. How would you calculate a good tip for a 55 dollar meal?
+print (55*.15)
+
+#2. Try adding a string and an integer with the + operator. What happens?
+#print ("corey" + 69)
+#It doesnt't work
+print ("corey" + str(69))
+
+#3. Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+print("45628 times 7839 equals {}" .format(45628*7839))
+
+#4. What's the value of the expression (10 < 20 and 30 < 20) or not(10 == 11)? 
+print (10 < 20 and 30 < 20) or not(10 == 11)
