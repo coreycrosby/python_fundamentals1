9a6895f6dee0194454d59e0970efb9c14facc904
Corey Crosby <ccc_13@live.ca>
2019-05-06 13:33:06 -0400

    exercise 3 completed

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,26 @@
 print ("What is your name?")
+user_name = input()
+print ("Hello {}".format(user_name))
+
+secret_password = "please"
+
+print ("What's the password?")
+password_attempt = input()
+
+correct_or_not = (password_attempt == secret_password)
+
+print ("That's {}!".format(correct_or_not))
+
+print ("How many cookies have been eaten?")
+eaten = input()
+
+remaining_cookies = 12 - int(eaten)
+
+print("There are {} cookies left.".format(remaining_cookies))
+
+print("How old are you?")
+user_age = input()
+
+user_birth_year = 2019 - int(user_age)
+
+print("You were born in {}".format(user_birth_year))
